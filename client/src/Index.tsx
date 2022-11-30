7c81aba68f1a2a5253b8d057e86770235da3cba4
Carlos <75898257+Carlos03s@users.noreply.github.com>
2022-11-30 15:18:50 -0300

    .

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -1,15 +1,16 @@
 import * as ReactDOM from 'react-dom';
 import Rotas from './Routes';
 import Cadastro from './cadastros/Cadastro';
+import Caixa from './caixa/Caixa'
 
 function render() {
     ReactDOM.render(
       <div>
         {/* <Rotas/> */}
-        <Cadastro/>
+        {/* <Cadastro/> */}
+        <Caixa/>
       </div>, document.getElementById('root')!);
   }
   
   render();
 
-  

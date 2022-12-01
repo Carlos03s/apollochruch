172aefb9ec38180ae045bf602facd09197773f27
anapaulamartinssouza <anp12.ap@gmail.com>
2022-12-01 14:39:56 -0300

    vaaai
    Vai essa merda

--- client/src/caixa/Caixa.tsx
+++ client/src/caixa/Caixa.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import './Caixa.css'
+import * as React from 'react'
+import {MainContainer} from './container/CaixaContent'
 
 export default function Caixa() {
   return (
-    <div>Caixa</div>
+  <MainContainer>
+    <h1>Caixa</h1>
+   <div></div> 
+  </MainContainer>
+    
   )
 }

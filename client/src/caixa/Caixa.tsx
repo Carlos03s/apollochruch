f2a9bbdd195f88e1214ef64072c3024556b25992
anapaulamartinssouza <anp12.ap@gmail.com>
2022-12-01 16:02:19 -0300

    Tabelas
    Template das tabelas

--- client/src/caixa/Caixa.tsx
+++ client/src/caixa/Caixa.tsx
@@ -4,8 +4,43 @@ import {MainContainer} from './container/CaixaContent'
 export default function Caixa() {
   return (
   <MainContainer>
-    <h1>Caixa</h1>
-   <div></div> 
+   <div className='tabela1'>
+    <h1 className='h1'>Caixa resumido</h1>
+      <div className='dropdown'>
+        <select name="select" id='a'>
+          <option value="valor1">Últimos 1 dias</option>
+          <option value="valor2" selected>Últimos 7 dias</option>
+          <option value="valor3">Últimos 30 dias</option>
+      </select>
+      </div>
+      <button className='adicionar'>Adicionar novo caixa
+</button>
+    
+  </div> 
+   <div className='tabela2'>
+    <h2 className='h2'>Caixa detalhado</h2>
+    <div className='dropdown'>
+        <select name="select" id='b'>
+          <option value="valor1">Últimos 1 dias</option>
+          <option value="valor2" selected>Últimos 7 dias</option>
+          <option value="valor3">Últimos 30 dias</option>
+      </select>
+      </div>
+    
+   </div> 
+   <div className='extrato'>
+    <h3 className='h3'>Extrato</h3>
+    <div className='dropdown'>
+        <select name="select" id='c'>
+          <option value="valor1">Últimos 1 dias</option>
+          <option value="valor2" selected>Últimos 7 dias</option>
+          <option value="valor3">Últimos 30 dias</option>
+      </select>
+      </div>
+   </div>
+  
+
+   
   </MainContainer>
     
   )
--- client/src/caixa/container/CaixaContent.tsx
+++ client/src/caixa/container/CaixaContent.tsx
@@ -1,6 +1,113 @@
 import styled from 'styled-components'
 
 export const MainContainer = styled.div`
-  
-    
+height  : 100%;
+width: 100%;
+
+.tabela1{
+position: absolute;
+width: 360px;
+height: 534px;
+left: 50px;
+top: 95px;
+background: #E5EAEE;
+border-radius: 30px;
+}
+
+h1{
+position: absolute;
+width: 175px;
+height: 27px;
+left: 17px;
+top: 17px;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 600;
+font-size: 24px;
+line-height: 28px;
+}
+
+#a{
+position: absolute;
+width: 125px;
+height: 25px;
+right: 20px;
+top: 18px;
+background: #C9D2DB;
+border-radius: 15px;
+}
+
+.tabela2{
+position: absolute;
+width: 853px;
+height: 534px;
+left: 428px;
+top: 95px;
+background: #E5EAEE;
+border-radius: 30px;
+}
+
+h2{
+position: absolute;
+width: 175px;
+height: 27px;
+left: 17px;
+top: 17px;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 600;
+font-size: 24px;
+line-height: 28px;
+}
+
+.extrato{
+position: absolute;
+width: 360px;
+height: 169px;
+left: 50px;
+top: 656px;
+background: #E5EAEE;
+border-radius: 30px;
+}
+
+h3{
+position: absolute;
+width: 175px;
+height: 27px;
+left: 17px;
+top: 17px;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 600;
+font-size: 24px;
+line-height: 28px;   
+}
+.adicionar{
+position: absolute;
+width: 275px;
+height: 52px;
+left: 41px;
+top: 456px;
+background: #C9D2DB;
+border-radius: 10px;
+border-color: #C9D2DB;
+}
+#b{
+position: absolute;
+width: 125px;
+height: 25px;
+right: 20px;
+top: 18px;
+background: #C9D2DB;
+border-radius: 15px;   
+}
+#c{
+position: absolute;
+width: 125px;
+height: 25px;
+right: 20px;
+top: 18px;
+background: #C9D2DB;
+border-radius: 15px; 
+}
 `

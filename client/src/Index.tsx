d79f6ad83a07cb3ef2223b675d48d7913ac955b5
Alexsander P.F. Santos <82063944+Alexsander33set@users.noreply.github.com>
2022-12-05 18:05:19 -0300

    Donors_and_collaborators route

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -1,14 +1,16 @@
 import * as ReactDOM from 'react-dom';
 import Rotas from './Routes';
 import Cadastro from './cadastros/Cadastro';
-import Caixa from './caixa/Caixa'
+import Caixa from './caixa/Caixa';
+import Donors_and_collaborators from './donors_and_collaborators/Donors_and_collaborators';
 
 function render() {
     ReactDOM.render(
       <div>
         {/* <Rotas/> */}
         <Cadastro/>
-        <Caixa/> 
+        <Caixa/>
+        <Donors_and_collaborators/>
       </div>, document.getElementById('root')!);
   }
   
